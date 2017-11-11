5a0652c60bebfb3a761aa0de584318f639d3de83
robyhuzwandar <robyhuzwandar@gmail.com>
2017-11-11 14:13:42 +0800

    revisi spinner

--- app/src/main/java/com/codetairy/logintanpadatabase/HalamanDaftar.java
+++ app/src/main/java/com/codetairy/logintanpadatabase/HalamanDaftar.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class HalamanDaftar extends AppCompatActivity {
-    String nama, nim, alamat;
+    String nama, nim, alamat, jurusan;
     EditText txt_nama, txt_nim, txt_alamat;
+    Spinner sp_jurusan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +22,18 @@ public class HalamanDaftar extends AppCompatActivity {
         txt_nama = (EditText)findViewById(R.id.txt_nama);
         txt_nim = (EditText)findViewById(R.id.txt_nim);
         txt_alamat = (EditText)findViewById(R.id.txt_alamat);
+        sp_jurusan = (Spinner)findViewById(R.id.spinner);
 
         nama = txt_nama.getText().toString();
         nim = txt_nim.getText().toString();
         alamat = txt_alamat.getText().toString();
+        jurusan = sp_jurusan.getSelectedItem().toString();
 
         Intent i = new Intent(HalamanDaftar.this, HalamanTampil.class);
         i.putExtra("parse_nama", nama );
         i.putExtra("parse_nim", nim );
         i.putExtra("parse_alamat", alamat );
-//        Bundle b = new Bundle();
-//        b.putString("Parse_nama", nama);
-//        b.putString("Parse_nim", nim);
-//        b.putString("Parse_alamat", alamat);
-//        i.putExtras(b);
+        i.putExtra("parse_jurusan", jurusan);
         startActivity(i);
     }
 }
--- app/src/main/java/com/codetairy/logintanpadatabase/HalamanTampil.java
+++ app/src/main/java/com/codetairy/logintanpadatabase/HalamanTampil.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class HalamanTampil extends AppCompatActivity {
-    String nama, nim, alamat;
-    TextView txt_nama, txt_nim, txt_alamat;
+    String nama, nim, alamat, jurusan;
+    TextView txt_nama, txt_nim, txt_alamat, txt_jurusan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +16,16 @@ public class HalamanTampil extends AppCompatActivity {
         txt_nama = (TextView)findViewById(R.id.txt_namaT);
         txt_nim = (TextView)findViewById(R.id.txt_nimT);
         txt_alamat = (TextView)findViewById(R.id.txt_alamatT);
+        txt_jurusan = (TextView)findViewById(R.id.txt_jurusanT);
+
         nama = getIntent().getStringExtra("parse_nama");
         nim = getIntent().getStringExtra("parse_nim");
         alamat = getIntent().getStringExtra("parse_alamat");
+        jurusan = getIntent().getStringExtra("parse_jurusan");
 
         txt_nama.setText("Nama : " +nama);
         txt_nim.setText("Nim : " +nim);
         txt_alamat.setText("Alamat : " + alamat);
+        txt_jurusan.setText("Jurusan : " + jurusan);
     }
 }
